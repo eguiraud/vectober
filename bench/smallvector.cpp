8d90f4f4f6c297b56cba287d8a6105d664f1d892
Enrico Guiraud <enrico.guiraud@cern.ch>
2020-11-11 10:33:56 +0100

    Make sure that the non-POD specialization of SmallVector compiles

--- bench/smallvector.cpp
+++ bench/smallvector.cpp
@@ -16,4 +16,19 @@ BENCHMARK_TEMPLATE(InvariantMasses, llvm::SmallVector<float, 8>)->Apply(GetArgum
 BENCHMARK_TEMPLATE(MaskingSimple, llvm::SmallVector<float, 8>)->Apply(GetArguments);
 BENCHMARK_TEMPLATE(MaskingComplex, llvm::SmallVector<float, 8>)->Apply(GetArguments);
 
+// trigger instantiation of SmallVector<NonPOD> to make sure it compiles
+void instantiate_nonpod_smallvector() {
+   struct S {
+      int x;
+      S() { x = 42; }
+      S(const S&) { x = 3; }
+      ~S() { x = 0; }
+   };
+
+   static_assert(!std::is_trivially_copy_constructible<S>::value, "");
+
+   llvm::SmallVector<S, 8> v;
+   v.push_back(S()); // trigger instantiation of `grow` for non-POD types
+}
+
 BENCHMARK_MAIN();
